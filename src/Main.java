c1b35ea286bdbd9169de4426cfb809d6dc5be2a9
larisarysbekova <user@email.com>
2019-08-16 13:36:09 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Box<Double, Integer> box1 = new Box<>(3.0, 2);
+        Box<Float, Float> box2 = new Box<>(4.0f, 1.0f);
+
+        addition(box1, box2);
+        multiplication(box1, box2);
+    }
+
+    public static void addition(Box<? extends Double, ? extends Integer> box1, Box<? extends Float, Float> box2) {
+            Number number = box1.getNumber1() + box1.getNumber2() + box2.getNumber1() + box2.getNumber2();
+
+            System.out.println("Коробка первая " + number);
+
+
+    }
+
+    public static void multiplication(Box<? extends Double, ? extends Integer> box1,
+                                      Box<? extends Float, Float> box2) {
+
+        Number number = box1.getNumber1() * box1.getNumber2() * box2.getNumber1() * box2.getNumber2();
+
+        System.out.println("Коробка вторая " + number);
+
+    }
+}
+
+
+
